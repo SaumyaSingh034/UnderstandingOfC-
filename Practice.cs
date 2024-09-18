56fe55b770d7d43875914c2f97919d3945c0dfa4
Saumya Singh <singhsaumyait034@gmail.com>
2024-09-18 14:35:18 +0530

    Understanding Data Types in C#

--- Practice.cs
+++ Practice.cs
@@ -1,9 +1,21 @@
 using System
 namespace LearningC#
 {
-    class Practice{
-        static void Main(string[] args){
-            Console.WriteLine("Hello World");
-        }
+    public class Practice{
+    static void Main(String[] args){
+        Console.WriteLine("Hello Word");
+        int a = 4;
+        Console.WriteLine($"Printining Integer Value {a}");
+        String str = "Saumya";
+        Console.WriteLine($"Hello {str}");
+        var age = 27;
+        Console.WriteLine($"Hello {age}");
+        //age = "Singh";
+        dynamic data = "Tryaksh Singh Bist";
+        Console.WriteLine($"Hello {data}");
+        data = 899.9999;
+        Console.WriteLine($"Hello {data}");
+        
     }
+}
 }
